77ca3e81ad9185969bf9387b1e7e93ef80057c65
Ryan LaBouve <imryanlabouve@gmail.com>
2016-04-29 13:07:26 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('App is running');
+});
